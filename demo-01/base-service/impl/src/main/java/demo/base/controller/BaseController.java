7f0664602915008a2985abb3116e2a2253e1e6cf
杨玉伟 <81285369@qq.com>
2017-07-03 17:24:49 +0800

    调整一处包命名错误

--- demo-01/base-service/impl/src/main/java/demo/base/controller/BaseController.java
+++ demo-01/base-service/impl/src/main/java/demo/base/controller/BaseController.java
@@ -1,6 +1,6 @@
 package demo.base.controller;
 
-import demo.complex.base.api.BaseAPI;
+import demo.base.api.BaseAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
