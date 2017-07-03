7f86457067fcd24a17c8a4acbc66b196431451dd
杨玉伟 <81285369@qq.com>
2017-07-03 17:04:53 +0800

    为Application类添加@EnableEurekaClient注解

--- demo-01/base-service/impl/src/main/java/demo/base/BaseServiceApplication.java
+++ demo-01/base-service/impl/src/main/java/demo/base/BaseServiceApplication.java
@@ -2,8 +2,10 @@ package demo.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BaseServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseServiceApplication.class, args);
