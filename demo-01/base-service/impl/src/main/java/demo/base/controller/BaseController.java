49d07ec3879c1b259a03abce1c30e8abcace7a49
杨玉伟 <81285369@qq.com>
2017-07-04 09:50:23 +0800

    调整注解

--- demo-01/base-service/impl/src/main/java/demo/base/controller/BaseController.java
+++ demo-01/base-service/impl/src/main/java/demo/base/controller/BaseController.java
@@ -3,6 +3,7 @@ package demo.base.controller;
 import demo.base.api.BaseAPI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
--- demo-01/base-service/interface/src/main/java/demo/base/api/BaseAPI.java
+++ demo-01/base-service/interface/src/main/java/demo/base/api/BaseAPI.java
@@ -2,6 +2,7 @@ package demo.base.api;
 
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient("base-service")
 @RequestMapping(value = "/base", produces = MediaType.APPLICATION_JSON_VALUE)
 public interface BaseAPI {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    // @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @GetMapping("/hello")
     String hello(@RequestParam("user") String user);
 }
